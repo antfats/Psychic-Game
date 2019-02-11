e9ecd72414e2f96073b6b75abe6e33d668d9d0f0
antfats <antfats@hotmail.com>
2019-02-10 21:18:25 -0700

    first ocmmit

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+//create variables for wins/losses
+//have the computer picks a letter
+//take input from the user, and store that input
+//compare the input the user typed, with what the computer picked
+
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+var compGuess;
+var userGuess;
+
+function computerGuess(){
+    document.getElementById(getRandomLetter);
+    
+
+}
+
