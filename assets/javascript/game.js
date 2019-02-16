966278d0c859f5d301a9494b589a032277912f08
antfats <antfats@hotmail.com>
2019-02-15 17:18:29 -0700

    Almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,20 +2,35 @@
 //have the computer picks a letter
 //take input from the user, and store that input
 //compare the input the user typed, with what the computer picked
+$(document).ready(); {
+    var wins = 0;
+    var losses = 0;
+    var guesses = 10;
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-var wins = 0;
-var losses = 0;
-var guesses = 10;
-var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
-    'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-var compGuess;
-var userGuess;
-
-    console.log(computerGuess)
+    var userGuess;
 
 
-    function computerGuess() {
+    //User presses a key and choses a letter. the computer randomly picks a number and then compares the two. 
+    
+    document.onkeypress = function (event) {
+        var userGuess = event.key;
+        var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length )];
+        $("#guesses").append(userGuess);
+        $("#compGuess").append(computerGuess);
+
+    }
+    //win/lose conditions
+
+    if (userGuess === computerGuess){
+        alert("YOU WIN!!!");
+        wins++;
+
+
 
 
     }
+
+
+}
